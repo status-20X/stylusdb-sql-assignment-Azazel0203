3bf6c8e0b49054c1023216826d3e9ca5a09a0593
Azazel0203 <aadarshkr.singh.cd.ece21@itbhu.ac.in>
2024-04-12 12:58:04 +0530

    test1-4

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-async function readCSV(filePath) {
-    const data = [];
+function readCSV(filePath) {
+    const results = [];
+
     return new Promise((resolve, reject) => {
         fs.createReadStream(filePath)
             .pipe(csv())
-            .on('data', (row) => {
-                data.push(row);
-            })
+            .on('data', (data) => results.push(data))
             .on('end', () => {
-                resolve(data);
+                resolve(results);
             })
             .on('error', (error) => {
                 reject(error);
